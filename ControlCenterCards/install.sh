dcacae1028b66513440974cdd4c7cc374d5993d1
Qvarta <qvarta77@gmail.com>
2026-02-07 14:17:23 +0300

    On branch main

--- ControlCenterCards/install.sh
+++ ControlCenterCards/install.sh
@@ -4,12 +4,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 QUICKSHELL_CONFIG="$HOME/.config/quickshell"
 NOCTALIA_SHELL="$QUICKSHELL_CONFIG/noctalia-shell"
 
+WEATHER_CARD_DEST="$NOCTALIA_SHELL/Modules/Cards/WeatherVerticalCard.qml"
 SYS_MONITOR_CARD_DEST="$NOCTALIA_SHELL/Modules/Cards/SysMonitorCard.qml"
+LOCATION_SERVICE_DEST="$NOCTALIA_SHELL/Services/Location/LocationService.qml"
 CONTROL_CENTER_PANEL_DEST="$NOCTALIA_SHELL/Modules/Panels/ControlCenter/ControlCenterPanel.qml"
 CONTROL_CENTER_TAB_DEST="$NOCTALIA_SHELL/Modules/Panels/Settings/Tabs/ControlCenter/ControlCenterTab.qml"
 
 if [ ! -d "$NOCTALIA_SHELL" ]; then
-    echo "Ошибка: не найдена noctalia-shell в ~/.config/quickshell"
+	echo "Ошибка: не найдена noctalia-shell в ~/.config/quickshell"
     exit 1
 fi
 
@@ -17,7 +19,6 @@ copy_file() {
     local source="$1"
     local destination="$2"
     local description="$3"
-    local file_type="$4"
     
     if [ ! -f "$source" ]; then
         echo "Ошибка: $(basename "$source") не найден в папке со скриптом!"
@@ -43,11 +44,19 @@ copy_file "$SCRIPT_DIR/SysMonitorCard.qml" "$SYS_MONITOR_CARD_DEST" "1. Доба
 
 echo "----------------------------------------"
 
-copy_file "$SCRIPT_DIR/ControlCenterPanel.qml" "$CONTROL_CENTER_PANEL_DEST" "2. Замена ControlCenterPanel.qml" "заменен"
+copy_file "$SCRIPT_DIR/WeatherVerticalCard.qml" "$WEATHER_CARD_DEST" "1. Добавление WeatherVerticalCard.qml"
 
 echo "----------------------------------------"
 
-copy_file "$SCRIPT_DIR/ControlCenterTab.qml" "$CONTROL_CENTER_TAB_DEST" "3. Замена ControlCenterTab.qml" "заменен"
+copy_file "$SCRIPT_DIR/LocationService.qml" "$LOCATION_SERVICE_DEST" "2. Замена LocationService.qml"
+
+echo "----------------------------------------"
+
+copy_file "$SCRIPT_DIR/ControlCenterPanel.qml" "$CONTROL_CENTER_PANEL_DEST" "3. Замена ControlCenterPanel.qml"
+
+echo "----------------------------------------"
+
+copy_file "$SCRIPT_DIR/ControlCenterTab.qml" "$CONTROL_CENTER_TAB_DEST" "4. Замена ControlCenterTab.qml"
 
 echo "========================================"
 echo "Все операции завершены!"
